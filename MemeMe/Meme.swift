8b98f360e94165945f981b3e6e29c6aaac348107
Tobias Helmrich <me@tobias-helmrich.de>
2016-08-13 17:21:27 +0200

    Create Meme type

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Tobias Helmrich on 12.08.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let image: UIImage
+    let memedImage: UIImage
+}

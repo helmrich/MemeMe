8d5d2da2217b912a4dc44f6ab671098ce49acd51
Tobias Helmrich <me@tobias-helmrich.de>
2016-08-30 11:45:37 +0200

    Refactor code

--- MemeMe/Meme.swift
+++ MemeMe/Meme.swift
@@ -23,4 +23,28 @@ struct Meme {
         (UIApplication.sharedApplication().delegate as! AppDelegate).memes.append(meme)
     }
     
+    func share(withShareButton shareButton: UIBarButtonItem, onViewController viewController: UIViewController) {
+        // - Create an instance of a UIActivityViewController
+        let activityViewController = UIActivityViewController(activityItems: [self.memedImage], applicationActivities: nil)
+        
+        // - Call the saveMeme() function in the activity view controller's completion handler after optionally unwrapping
+        // the text fields' and imageView's values
+        activityViewController.completionWithItemsHandler = {
+            (activity, success, items, error) in
+            // If it was a success - which means that the action could be performed the whole editing view controller should be dismissed
+            if success {
+                Meme.saveMeme(self.topText, bottomText: self.bottomText, image: self.image, memedImage: self.memedImage)
+                viewController.dismissViewControllerAnimated(true, completion: nil)
+            } else {
+                // If it didn't succeed - which is also the case if the cancel button is pressed in the activity view controller -
+                // only the activity view controller should be dismissed, the editing view controller however should still be displayed
+                activityViewController.dismissViewControllerAnimated(true, completion: nil)
+            }
+        }
+        // The activityViewController's popoverPresentationController has to be set in order to work for iPads
+        activityViewController.popoverPresentationController?.barButtonItem = shareButton
+        // - Present the activity view controller
+        viewController.presentViewController(activityViewController, animated: true, completion: nil)
+    }
+    
 }

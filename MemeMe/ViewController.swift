da26f868032bd1e77e3a47edbcc5db604d3ac93f
Tobias Helmrich <me@tobias-helmrich.de>
2016-08-30 11:47:17 +0200

    Refactor code

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -57,9 +57,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // outlets' values are set to the existing meme's values
         // If it can't be unwrapped it means that no meme was passed so nothing should happen
         if let meme = meme {
+            memeImageView.image = meme.image
             memeTopTextField.text = meme.topText
             memeBottomTextField.text = meme.bottomText
-            memeImageView.image = meme.image
         }
         
         // Check if the used source types are available and
@@ -97,32 +97,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // - Generate a memed image
         let memedImage = generateMemedImage()
         
-        // - Create an instance of a UIActivityViewController
-        let activityViewController = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
-        
-        // - Call the saveMeme() function in the activity view controller's completion handler after optionally unwrapping
-        // the text fields' and imageView's values
-        activityViewController.completionWithItemsHandler = {
-            (activity, success, items, error) in
-            // If it was a success - which means that the action could be performed the whole editing view controller should be dismissed
-            if success {
-                if let topText = self.memeTopTextField.text,
-                    bottomText = self.memeBottomTextField.text,
-                    image = self.memeImageView.image {
-                    Meme.saveMeme(topText, bottomText: bottomText, image: image, memedImage: memedImage)
-                }
-                self.dismissViewControllerAnimated(true, completion: nil)
-            } else {
-                // If it didn't succeed - which is also the case if the cancel button is pressed in the activity view controller - 
-                // only the activity view controller should be dismissed, the editing view controller however should still be displayed
-                activityViewController.dismissViewControllerAnimated(true, completion: nil)
-            }
+        if let topText = memeTopTextField.text,
+        bottomText = memeBottomTextField.text,
+            image = memeImageView.image {
+            let meme = Meme(topText: topText, bottomText: bottomText, image: image, memedImage: memedImage)
+            meme.share(withShareButton: shareButton, onViewController: self)
         }
-        // The activityViewController's popoverPresentationController has to be set in order to work for iPads
-        activityViewController.popoverPresentationController?.barButtonItem = shareButton
-        // - Present the activity view controller
-        presentViewController(activityViewController, animated: true, completion: nil)
     }
+    
+    
     
     // This action will be triggered when the cancel button is pressed
     @IBAction func resetMeme() {
